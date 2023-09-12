189a83c28db99f966359a22b9df72e33e738ac71
Burrouwill <wgr.burroughs@gmail.com>
2023-09-12 12:13:43 +1200

    verifyJWT middleware implemented.

---
+++ Server/middleware/verifyJWT.js
@@ -0,0 +1,22 @@
+// Handles verification of JWT Tokens
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.sendStatus(401);
+    console.log(authHeader); // For testing: Should say "Bearer token"
+    const token = authHeader.split(' ')[1]; // Split auth header and get the token
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); // At this point we know we recieved a token, but something may have been tampered with --> Forbidden access response
+            req.user = decoded.username;
+            next();
+        }
+    );
+}
+
+module.exports = verifyJWT
