434db13d30cd71bc266008a93d2aeab80f72d154
DarkZek <marshall@rubyplaynetwork.com>
2023-09-13 14:04:58 +1200

    Added product editing and creation

--- Server/routes/view.js
+++ Server/routes/view.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var ProductController = require('../controllers/product');
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
+    const controller = new ProductController(res.locals.dburi,'products');
+    const product = await controller.getData(7);
     res.render("home", {
-        // Todo, replace this with array of actual `Boat[]` from db
+        // TODO:, replace this with array of actual `Boat[]` from db
         products: [
-            { id: 1, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 2, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 3, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 4, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 5, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 6, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 7, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 8, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 9, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 10, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
-            { id: 11, name: 'The Dutchman', description: 'The best boat', price: 14.99, imgUrl: '/assets/boats/The_Dutchman.png' },
+            product,
         ],
-        admin: true // Decide by middleware
+        admin: true // TODO: Decide by authorization
     });
 });
 
@@ -27,6 +20,16 @@ router.get('/login', function(req, res) {
 
 router.get('/register', function(req, res) {
     res.render("register", { layout: 'basic' });
+});
+
+router.get('/products/:productId/edit', async function(req, res) {
+    const controller = new ProductController(res.locals.dburi,'products');
+    const product = await controller.getData(req.params.productId);
+    res.render("product_edit", { product });
+});
+
+router.get('/products/new', async function(req, res) {
+    res.render("product_edit", { creation: true, product: { name: "New Product", imgUrl: "/assets/boats/Add.png", description: "Fill out description", price: 14.99 } });
 });
 
 module.exports = router;
