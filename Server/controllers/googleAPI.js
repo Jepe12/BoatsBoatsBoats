650aa24c4d2d76cb02d5c0ba38d94d74b40eb040
Jepe12 <101069131+Jepe12@users.noreply.github.com>
2023-09-28 09:30:25 +1300

    added url

--- Server/controllers/googleAPI.js
+++ Server/controllers/googleAPI.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/google/callback",
+  callbackURL: process.env.HOSTED_URL +"/google/callback",
   passReqToCallback: true
 }, async function (request, accessToken, refreshToken, profile, done) {
   try {
